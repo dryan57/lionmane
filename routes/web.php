17b2b0f8419fad84246a225a8dad9a6f8dc9c450
Bryan <bryan@gofablet.com>
2020-01-13 12:38:23 -0600

    Adding email controller route to routes file.

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::get('/', function () {
 
 Route::resource('contacts', 'ContactController');
 Route::resource('telephones', 'TelephoneController');
+Route::resource('emails', 'EmailController');
