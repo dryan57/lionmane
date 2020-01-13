46b4fb1b115040dfe97db9b3cf1a17f010436716
Bryan <bryan@gofablet.com>
2020-01-13 12:31:44 -0600

    Adding Migration file for email database table.

---
+++ database/migrations/2020_01_13_182659_create_emails_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('contact_id');
+            $table->foreign('contact_id')->references('id')->on('contacts');
+            $table->timestamps();
+            $table->string('email');
+            $table->string('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
