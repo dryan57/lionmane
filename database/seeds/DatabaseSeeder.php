fa0011062c95d451d1f47f099530a60e8258b37f
Bryan <bryan@gofablet.com>
2020-01-15 13:31:26 -0600

    Adding dump data to contact table.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('contacts')->insert([
+            'created_at' =>now(),
+            'updated_at' =>now(),
+            'first_name' =>'Bryan',
+            'last_name' => 'Alvarado',
+            'nick_name' => 'dryan57',
+            'dob' => '1988-12-28',
+            'gender' => 1,
+            'image' => '',
+            'active' => 1,
+        ]);
+        DB::table('contacts')->insert([
+            'created_at' =>now(),
+            'updated_at' =>now(),
+            'first_name' =>'Ramberto',
+            'last_name' => 'Roca',
+            'nick_name' => 'RRoca',
+            'dob' => '1990-10-05',
+            'gender' => 1,
+            'image' => '',
+            'active' => 1,
+        ]);
     }
 }
